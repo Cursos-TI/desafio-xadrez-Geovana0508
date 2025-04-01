10389b1ecbfadc829cbb6a0c73cb6c349b3ab405
Geovana0508 <geovanna.silveira16@gmail.com>
2025-04-01 21:18:03 +0000

    - Adicionado movimento do Cavalo com loops aninhados - Implementado o movimento do Cavalo seguindo a lógica em "L". - Utilizado um loop for para mover 2 casas para baixo. - Utilizado um loop while para mover 1 casa para a esquerda.

--- xadrez.c
+++ xadrez.c
@@ -3,8 +3,6 @@
 int main() {
     // Movimento da Torre (usando for)
     printf("Movimento da Torre:\n");
-    // A Torre se move em linha reta horizontal ou vertical
-    // Neste caso, moverá 5 casas para a direita
     for (int t = 0; t < 5; t++) {
         printf("Direita\n");
     }
@@ -12,8 +10,6 @@ int main() {
 
     // Movimento do Bispo (usando while)
     printf("Movimento do Bispo:\n");
-    // O Bispo se move na diagonal
-    // Aqui, ele se moverá 5 casas para cima e à direita
     int b = 0;
     while (b < 5) {
         printf("Cima, Direita\n");
@@ -23,13 +19,24 @@ int main() {
 
     // Movimento da Rainha (usando do-while)
     printf("Movimento da Rainha:\n");
-    // A Rainha pode se mover em todas as direções
-    // Neste caso, moverá 8 casas para a esquerda
     int r = 0;
     do {
         printf("Esquerda\n");
         r++;
     } while (r < 8);
-    
+    printf("\n");
+
+    // Movimento do Cavalo (usando loops aninhados)
+    printf("Movimento do Cavalo:\n");
+    int i;
+    for (i = 0; i < 2; i++) { // Move 2 casas para baixo
+        printf("Baixo\n");
+    }
+    int j = 0;
+    while (j < 1) { // Move 1 casa para a esquerda
+        printf("Esquerda\n");
+        j++;
+    }
+
     return 0;
 }
